906b7c85bfb4e5f9ad5fda49a506e3bb4bbac56a
gas <fireworld116@gmail.com>
2021-06-10 18:03:06 +0300

    created and described table for placement of information about chats

---
+++ web/database/migrations/2021_06_10_145054_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('chat')->nullable()->comment('chat link');
+            $table->string('chat_name', 30)->nullable()->comment('the name of the chat provided by Google contacts');
+            $table->foreignUuid('contact_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
