7178a74fd35e5b0275578c548fddcd5cbb83831e
gas <fireworld116@gmail.com>
2021-06-11 10:30:05 +0300

    created and described model Chat

---
+++ web/app/Models/Chat.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Models;
+
+use App\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chat
+{
+    use HasFactory;
+    use UuidTrait;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'chat',
+        'chat_name',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
+}
