78ec7d75d7d12a582975725dffd72b4dff9c4edc
gas <fireworld116@gmail.com>
2021-06-11 10:28:31 +0300

    created and described model Site

---
+++ web/app/Models/Site.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Models;
+
+use App\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Site extends Model
+{
+    use HasFactory;
+    use UuidTrait;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'site',
+        'site_type',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
+}
