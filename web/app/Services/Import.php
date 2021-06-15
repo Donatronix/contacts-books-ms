a451da4adc59f9a055afc50f9de7272ac08844ec
gas <fireworld116@gmail.com>
2021-06-15 10:10:37 +0300

    debug

--- web/app/Services/Import.php
+++ web/app/Services/Import.php
@@ -8,11 +8,18 @@ use Illuminate\Http\UploadedFile;
 
 class Import
 {
+    // Temporary method for tests.
     public function run(Request $request)
     {
         return view("tests.import1");
     }
 
+    /**
+     *  Loops through the possible parsing options and returns an array in the desired format, if possible.
+     *
+     * @param Request $request
+     * @return array $data_result | false
+     */
     public function check(Request $request)
     {
         // get list classes by path app/Services/Imports/
@@ -23,18 +30,33 @@ class Import
         $file_data = $this->readFile($request);
         // trying to parse the contents of the file
         $data_result = $this->parse($file_data, $classes) ?? false;
-        dd($data_result);
+        dd($data_result); // TODO: Remove when finished working with parsing of all possible formats.
         return $data_result;
     }
 
+    /**
+     *  Reads the downloaded file and returns its contents.
+     *
+     * @param $request
+     * @return false|string
+     */
     public function readFile($request)
     {
+        // TODO: Validation !!!
+
         $file = $request->file('contacts');
         $path_file = $file->getPathname();
 
         return file_get_contents($path_file) ?? false;
     }
 
+    /**
+     *  Loops through the classes from the imports directory, trying to find the required file format and, if it finds it, tries to parse it.
+     *
+     * @param string $file_data
+     * @param array $classes
+     * @return false|array $data_result
+     */
     public function parse($file_data, $classes)
     {
         $data_result = false;
@@ -46,8 +68,7 @@ class Import
                 $path_to_class = $path_to_dir . $class;
                 $data_object = new $path_to_class($file_data);
                 $data_check = $this->checkArrayByEmpty((array)$data_object);
-                if($data_check == null)
-                {
+                if($data_check == null){
                     unset($data_object);
                 }
                 else{
@@ -59,20 +80,23 @@ class Import
                 break;
             }
         }
-        dump($data_result);
-        die('END');
 
         return $data_result ?? false;
     }
 
+    /**
+     *  Reads the class import directory and creates an array from them.
+     *
+     * @param string $dirpath
+     * @return array $result
+     */
     public function getClassList($dirpath)
     {
         $result = [];
         $cdir = scandir($dirpath);
         foreach ($cdir as $value)
         {
-            if (!in_array($value,array(".", "..")) && !is_dir($dirpath . DIRECTORY_SEPARATOR . $value))
-            {
+            if (!in_array($value,array(".", "..")) && !is_dir($dirpath . DIRECTORY_SEPARATOR . $value)){
                 $value = trim(substr($value, 0, -4));
                 $result[] = $value;
             }
@@ -80,6 +104,12 @@ class Import
         return $result;
     }
 
+    /**
+     *   Loops through an array to determine an internal empty element.
+     *
+     * @param array $list
+     * @return mixed
+     */
     public function checkArrayByEmpty($list)
     {
         foreach ($list as $value){
